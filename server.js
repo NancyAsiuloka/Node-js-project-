82ddbf59c14b1622d8f4cdc8ce938943466f2268
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-29 10:12:23 +0100

    logged some modules to the console

--- server.js
+++ server.js
@@ -13,3 +13,6 @@ console.log(__filename)
 console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
+
+console.log(path.parse(__filename))
+
