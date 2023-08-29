818f30c4ca843578422f8b94cdc49940ba1f8e31
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-29 10:06:46 +0100

    logged some modules to the console

---
+++ server.js
@@ -0,0 +1,9 @@
+// console.log(global);
+
+const os = require('os');
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname)
+console.log(__filename)
