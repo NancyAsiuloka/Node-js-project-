a735556a57616e434abdfa6bf0a28c093bc6bf52
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-31 11:00:09 +0100

    renamed a file

---
+++ server.js
@@ -0,0 +1,9 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('log', (msg) => logEvents(msg));
