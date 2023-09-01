984bdb4660acab204da85882f1d904dc61528507
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-01 09:23:23 +0100

    added a comment

--- logEvent.js
+++ logEvent.js
@@ -1,3 +1,3 @@
-const fs = require('fs');
-const fsPromises = require('fs').promise;
-const path = require('path');
+// const fs = require('fs');
+// const fsPromises = require('fs').promise;
+// const path = require('path');
