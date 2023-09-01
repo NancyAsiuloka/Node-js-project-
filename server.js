8c5b461dc1af6796f869aed1b03ca5567f5ddc61
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-01 09:24:16 +0100

    added a comment

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter {};
-// Initialize Object
-const myEmitter = new Emitter();
-// add listener for the log event
-myEmitter.on('log', (msg) => logEvents(msg));
-// Emit event
-myEmitter.emit('log', 'Log event emitted');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get('^/$|/index.html', (req, res) => {
+    // res.sendFile('./views/index.html', { root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+app.get('/new-page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
