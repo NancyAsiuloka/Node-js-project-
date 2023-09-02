77a69caabbd85ef2b87f169dad64657fa3c00db5
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-02 11:52:11 +0100

    added a comment

--- server.js
+++ server.js
@@ -1,14 +1,75 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index.html', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname});
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
-app.get('/new-page.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
+// custom middleware logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+// Cross origin resource sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.includes(origin) !== -1 || !origin){
+            callback(null, true);
+        } else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// Middlewares
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/subdir', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+}, (req, res) => {
+    res.send('Hello World')
+});
+
+// Chaining routes handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+};
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+};
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+};
+app.get('/chain(.html)?', [one, two, three]);
+
+// app.use does not accept regex
+// app.all is more for routing all http methods at once
+app.all('/*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')){
+        res.json({ error: "404 Not Found" });
+    } else{
+        res.type('txt').send('404 Not Found');
+    }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
