c67483af83ebb2b41c381e5232df755dfaab4fba
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-04 12:21:03 +0100

    chained routes

---
+++ routes/api/employees.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.router();
+const path = require('path');
+const data = {};
+data.employees = require('../../data/employees.json')
+
+router.route('/')
+    .get((req, res) => {
+        res.json(data.employees);
+    })
+    .post((req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname
+        });
+    })
+    .put((req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname
+        });
+    })
+    .delete((req, res) => {
+        res.json({ "id": req.body.id});
+    });
+
+module.exports = router
