f6401344720f2de40b39b4212a41d38c3a984618
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-05 09:06:43 +0100

    added a comment

--- server.js
+++ server.js
@@ -2,72 +2,48 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOption')
 const PORT = process.env.PORT || 3500;
 
-// custom middleware logger
+// Custom middleware logger
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path}`);
     next();
 });
 
-// Cross origin resource sharing
-const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
-const corsOptions = {
-    origin: (origin, callback) => {
-        if(whitelist.includes(origin) !== -1 || !origin){
-            callback(null, true);
-        } else{
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    optionsSuccessStatus: 200
-};
+// CORS
 app.use(cors(corsOptions));
 
-// Middlewares
-app.use(express.urlencoded({ extended: false}))
+// Built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+// Built-in middleware for JSON
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '/public')))
-app.use('/subdir', express.static(path.join(__dirname, '/public')))
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
 app.use('/employees', require('./routes/api/employees'));
-
 
 // Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
+    console.log('Attempted to load hello.html');
     next();
 }, (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World');
 });
 
-// Chaining routes handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-};
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-};
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-};
-app.get('/chain(.html)?', [one, two, three]);
-
-// app.use does not accept regex
-// app.all is more for routing all http methods at once
+// Handle 404 errors
 app.all('/*', (req, res) => {
-    res.status(404)
-    if(req.accepts('html')){
+    res.status(404);
+    if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if(req.accepts('json')){
-        res.json({ error: "404 Not Found" });
-    } else{
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
         res.type('txt').send('404 Not Found');
     }
 });
