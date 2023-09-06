4a0f3329c170a4881ded861b7a34d98bc8ef3492
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-06 14:13:36 +0100

    added some changes

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
-const verifyJWT = require('../../middleware/verifyJWT');
+
 
 // Employees Collection Routes
 router.route('/')
-    .get(verifyJWT, employeesController.getAllEmployees)
+    .get(employeesController.getAllEmployees)
     .post(employeesController.createNewEmployee)
     .put(employeesController.updateEmployee)
     .delete(employeesController.deleteEmployee);
