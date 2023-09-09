e64bf680af860f11ef8d56fd2828c41936b4887e
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-09 05:45:26 +0100

    created a new file

---
+++ model/Employee.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+    firstname: {
+        type: 'string',
+        required: true
+    },
+    lastname: {
+        type: 'string',
+        required: true
+    }
+})
