7517b969d8ec46e324c24d3d8371944338b3529b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-09 07:07:38 +0100

    added some changes in the employeesController

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -14,16 +14,26 @@ const createNewEmployee = async (req, res) => {
         }
 
     try {
-
+        const result = await Employee.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        });
+        res.status(201).json(result);
     } catch (err) {
         console.log(err);
     }
 };
 
-const updateEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const updateEmployee =  async (req, res) => {
+    if(!req?.body?.id){
+        return res.status(400)
+        .json({ message: 'ID parameter id required.' });
+    }
+
+    const employee = await Employee.findOne({ _id: req.body.id }).exec();
     if (!employee) {
-        return res.status(400).json({"message": `Employee ID ${req.body.id} not found` });
+        return res.status(204)
+        .json({"message": `No employee matches ${req.body.id}` });
     }
     if (req.body.firstname) employee.firstname = req.body.firstname;
     if (req.body.lastname) employee.lastname = req.body.lastname;
