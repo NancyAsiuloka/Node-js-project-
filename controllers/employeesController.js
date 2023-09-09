5894b25cf24a5e41d893d57b3bd33775c0590736
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-09 13:57:09 +0100

    added some changes in the employeesController

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,92 +1,95 @@
-const Employee = require('../model/Employee');
 
-const getAllEmployees = async (req, res) => {
-        const employees = await Employee.find(); // Use `find` to get all employees
-        if (!employees) {
-            return res.status(204).json({ message: 'No employees found' });
-        }
-        res.json(employees);
-};
+ const Employee = require('../model/Employee');
+ console.log(Employee);
 
-const createNewEmployee = async (req, res) => {
-    if (!req.body.firstname || !req.body.lastname) {
-        return res.status(400).json({ message: 'First and last names are required' });
-    }
+ const getAllEmployees = async (req, res) => {
+         const employees = await Employee.find(); // Use `find` to get all employees
+     console.log(employees)
+         if (!employees) return res.status(204)
+         .json({ message: 'No employees found' });
 
-    try {
-        const result = await Employee.create({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname
-        });
-        res.status(201).json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
+         res.json(employees);
+ };
 
-const updateEmployee = async (req, res) => {
-    if (!req.body.id) {
-        return res.status(400).json({ message: 'ID parameter id required' });
-    }
+ const createNewEmployee = async (req, res) => {
+     if (!req.body.firstname || !req.body.lastname) {
+         return res.status(400).json({ message: 'First and last names are required' });
+     }
 
-    try {
-        const employee = await Employee.findOne({ _id: req.body.id });
-        if (!employee) {
-            return res.status(204).json({ message: `No employee matches ${req.body.id}` });
-        }
+     try {
+         const result = await Employee.create({
+             firstname: req.body.firstname,
+             lastname: req.body.lastname
+         });
+         res.status(201).json(result);
+     } catch (err) {
+         console.error(err);
+         res.status(500).json({ message: 'Internal Server Error' });
+     }
+ };
 
-        if (req.body.firstname) employee.firstname = req.body.firstname;
-        if (req.body.lastname) employee.lastname = req.body.lastname;
-        const result = await employee.save();
-        res.json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
+ const updateEmployee = async (req, res) => {
+     if (!req.body.id) {
+         return res.status(400).json({ message: 'ID parameter id required' });
+     }
 
-const deleteEmployee = async (req, res) => {
-    if (!req.body.id) {
-        return res.status(400).json({ message: 'Employee Id required' });
-    }
+     try {
+         const employee = await Employee.findOne({ _id: req.body.id });
+         if (!employee) {
+             return res.status(204).json({ message: `No employee matches ${req.body.id}` });
+         }
 
-    try {
-        const employee = await Employee.findOne({ _id: req.body.id });
-        if (!employee) {
-            return res.status(204).json({ message: `No employee matches ${req.body.id}` });
-        }
+         if (req.body.firstname) employee.firstname = req.body.firstname;
+         if (req.body.lastname) employee.lastname = req.body.lastname;
+         const result = await employee.save();
+         res.json(result);
+     } catch (err) {
+         console.error(err);
+         res.status(500).json({ message: 'Internal Server Error' });
+     }
+ };
 
-        const result = await employee.deleteOne({ _id: req.body.id });
-        res.json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
+ const deleteEmployee = async (req, res) => {
+     if (!req.body.id) {
+         return res.status(400).json({ message: 'Employee Id required' });
+     }
 
-const getEmployee = async (req, res) => {
-    if (!req.params.id) {
-        return res.status(400).json({ message: 'Employee Id required' });
-    }
+     try {
+         const employee = await Employee.findOne({ _id: req.body.id });
+         if (!employee) {
+             return res.status(204).json({ message: `No employee matches ${req.body.id}` });
+         }
 
-    try {
-        const employee = await Employee.findOne({ _id: req.params.id });
-        if (!employee) {
-            return res.status(204).json({ message: `No employee matches ${req.params.id}` });
-        }
+         const result = await employee.deleteOne({ _id: req.body.id });
+         res.json(result);
+     } catch (err) {
+         console.error(err);
+         res.status(500).json({ message: 'Internal Server Error' });
+     }
+ };
 
-        res.json(employee);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
+ const getEmployee = async (req, res) => {
+     if (!req.params.id) {
+         return res.status(400).json({ message: 'Employee Id required' });
+     }
 
-module.exports = {
+     try {
+         const employee = await Employee.findOne({ _id: req.params.id });
+         if (!employee) {
+             return res.status(204).json({ message: `No employee matches ${req.params.id}` });
+         }
+
+         res.json(employee);
+     } catch (err) {
+         console.error(err);
+         res.status(500).json({ message: 'Internal Server Error' });
+     }
+ };
+
+ module.exports = {
     getAllEmployees,
-    createNewEmployee,
-    updateEmployee,
-    deleteEmployee,
-    getEmployee
-};
+     createNewEmployee,
+     updateEmployee,
+     deleteEmployee,
+     getEmployee
+ };
