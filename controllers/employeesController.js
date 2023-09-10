00fe6d5f5084117aa50efa19fcf79b0ed49e9083
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-10 18:07:51 +0100

    added some changes

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -2,7 +2,7 @@
  const Employee = require('../model/Employee');
 
  const getAllEmployees = async (req, res) => {
-         const employees = await Employee.find(); // Use `find` to get all employees
+         const employees = await Employee.find();
          if (!employees) return res.status(204)
          .json({ message: 'No employees found' });
 
